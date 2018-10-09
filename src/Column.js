a80e25a46ac6d674a69fe6e106d6725907d08831
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-09 13:51:13 -0600

    task and column styled

--- src/Column.js
+++ src/Column.js
@@ -4,8 +4,8 @@ import Task from './Task'
 
 const Container = styled.div`
   margin: 8px;
-  border: 1px solidblack;
-  border-radius: 2px;
+  border: 1px solid lightgrey;
+  border-radius: 2px
 `;
 
 const Title = styled.h3`
--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``;
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  padding: 8px
+`;
 
 export default class Task extends React.Component {
   render() {
